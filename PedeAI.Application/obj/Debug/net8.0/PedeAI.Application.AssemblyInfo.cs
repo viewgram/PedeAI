99f46246529e939973632ff53edf9c8cb0589da2
linux_arthu <arthur.valente333@gmail.com>
2024-10-27 22:18:52 -0300

    removendo pragas

--- PedeAI.Application/obj/Debug/net8.0/PedeAI.Application.AssemblyInfo.cs
+++ PedeAI.Application/obj/Debug/net8.0/PedeAI.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PedeAI.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ec71f5856caa604ba181f875cf7f0d29a2bf8b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("PedeAI.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PedeAI.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
